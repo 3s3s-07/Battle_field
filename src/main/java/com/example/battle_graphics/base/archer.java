92602a3f6db78b343316d67f87900c22cf4876ae
bouthina moh. <bosywahab818@gmail.com>
2025-12-02 19:06:06 +0200

    12

---
+++ src/main/java/com/example/battle_graphics/base/archer.java
@@ -0,0 +1,19 @@
+package com.example.battle_graphics.base;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+public class archer extends fighter {
+    public archer(double x, double y) {
+        super("Archer", 110, x, y, 4.0, new Bow(), true, 0, Color.FORESTGREEN);
+    }
+        public void createShape() {
+            fighterShape = new Polygon(0, 50, 25, 0,50, 50);
+            fighterShape.setFill(getFighterColor());
+        }
+        public void setPosition(double x, double y) {
+            if (fighterShape != null) {
+                fighterShape.setLayoutX(x);
+                fighterShape.setLayoutY(y);
+            }
+        }
+    }
+
