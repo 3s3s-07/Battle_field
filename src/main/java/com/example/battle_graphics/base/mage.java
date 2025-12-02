16a00477464d0e4a52cd9104f029a937236a23b2
3s3s <eali8242@gmail.com>
2025-12-02 18:53:40 +0200

    update

--- src/main/java/com/example/battle_graphics/base/mage.java
+++ src/main/java/com/example/battle_graphics/base/mage.java
@@ -4,20 +4,17 @@ import javafx.scene.paint.Color;
 public class Mage extends fighter {
 
     public Mage(double x, double y) {
-        // الوراثة: 90 HP، السلاح: FireballStaff، اللون: أرجواني
-        super(90, x, y, new FireballStaff(), Color.PURPLE);
-        setSpeed(4.5);
-        createShape(); // إنشاء الشكل الخاص فوراً
+        super("Mage", 150, x, y, 3, new Sword(), true, 0, Color.DARKRED);
+        createShape();
     }
 
-    // تطبيق إنشاء الشكل: دائرة (Circle)
     @Override
     public void createShape() {
         Circle circle = new Circle(25);
         circle.setFill(getFighterColor());
         circle.setStroke(Color.BLACK);
         this.fighterShape = circle;
-        // تعيين الموقع الأولي لشكل JavaFX
+
         this.fighterShape.setTranslateX(xPosition);
         this.fighterShape.setTranslateY(yPosition);
     }
