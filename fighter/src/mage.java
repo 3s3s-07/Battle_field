089795455d47d995461b27902405ae8ac3c48332
hana ahmed <hana.ahmedradwan123@gmail.com>
2025-12-02 11:57:23 +0200

    fighter.mage.java

---
+++ fighter/src/mage.java
@@ -0,0 +1,9 @@
+public class mage extends fighter {
+    public mage(){
+        super();
+        name="mage";
+        health=90;
+        speed=2.5;
+        currentweapon=new magicwand();
+    }
+}
---
+++ fighter/src/warrior.java
@@ -0,0 +1,13 @@
+public class warrior extends fighter {
+
+
+    public warrior(){
+      super();
+        name="warrior";
+        health=120;
+        speed= 3;
+        currentweapon= new swordgun();
+    }
+
+
+}
