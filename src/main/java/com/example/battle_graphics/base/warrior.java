b7619870795b48658360fa24ec353d80733ff6da
bouthina moh. <bosywahab818@gmail.com>
2025-12-02 18:50:07 +0200

    update

--- src/main/java/com/example/battle_graphics/base/warrior.java
+++ src/main/java/com/example/battle_graphics/base/warrior.java
@@ -1,15 +1,29 @@
 package com.example.battle_graphics.base;
-
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 public class warrior extends fighter {
+    public warrior() {}
+    public warrior(double x,double y){
+            super("Warrior", 150, x, y, 3, new Sword(), true, 0, Color.DARKRED);
 
-
-    public warrior(){
-      super();
-        name="warrior";
-        health=120;
-        speed= 3;
-        currentweapon= new swordgun();
+        }
+        @Override
+    public void createShape() {
+        fighterShape = new Rectangle(60, 80);
+        fighterShape.setFill(getFighterColor());
+        fighterShape.setStroke(Color.DARKRED);
     }
 
+    public void setPosition(double x, double y) {
+        if (fighterShape != null) {
+            fighterShape.setTranslateX(x);
+            fighterShape.setTranslateY(y);
+        }
+    }
 
+    public Rectangle getFighterShape() {
+        return fighterShape;
+    }
 }
+
+
