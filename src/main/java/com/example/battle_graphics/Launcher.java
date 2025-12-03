0e1772540b21def4985e1dd0c89bd1740e2fa604
3s3s <eali8242@gmail.com>
2025-12-03 14:38:38 +0200

    finished

--- src/main/java/com/example/battle_graphics/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.battle_graphics;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
