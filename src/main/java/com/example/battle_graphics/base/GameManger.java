fa3817e9c8fc75e64870d7a8553eaba22edba3e4
bouthina moh. <bosywahab818@gmail.com>
2025-12-04 00:59:51 +0200

    ....

--- src/main/java/com/example/battle_graphics/base/GameManger.java
+++ src/main/java/com/example/battle_graphics/base/GameManger.java
@@ -29,4 +29,39 @@ public class GameManger {
         gamePane.getChildren().addAll(player1.getSprite(), player2.getSprite());
 
         startLoop();   // start the game loop
-    }}
+    }
+    private void startLoop() {
+        gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+        gameLoop.start();
+    }
+
+    /** Called every frame */
+    private void update() {
+        handleMovement();
+        handleShooting();
+        updateProjectiles();
+        checkCollisions();
+        checkWinner();
+    }
+
+    /** Moves both players according to key presses */
+    private void handleMovement() {
+
+        // Player 1 movement (WASD)
+        if (input.isWPressed()) player1.moveUp();
+        if (input.isSPressed()) player1.moveDown();
+        if (input.isAPressed()) player1.moveLeft();
+        if (input.isDPressed()) player1.moveRight();
+
+        // Player 2 movement (Arrow Keys)
+        if (input.isUpPressed()) player2.moveUp();
+        if (input.isDownPressed()) player2.moveDown();
+        if (input.isLeftPressed()) player2.moveLeft();
+        if (input.isRightPressed()) player2.moveRight();
+    }
+}
