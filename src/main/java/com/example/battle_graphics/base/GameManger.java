e41c8275068abcc55e3a48d60f2499bb10d93d18
Farida <fm776911@gmail.com>
2025-12-04 01:56:29 +0200

    .

--- src/main/java/com/example/battle_graphics/base/GameManger.java
+++ src/main/java/com/example/battle_graphics/base/GameManger.java
@@ -32,35 +32,10 @@ public class GameManger {
         gameLoop.start();
     }
     private void update() {
-        handleMovement();
-        handleShooting();
+        input.handleMovement();
         updateProjectiles();
         checkCollisions();
         checkWinner();
-    }
-    private void handleMovement() {
-        if (input.isWPressed()) player1.moveUp();
-        if (input.isSPressed()) player1.moveDown();
-        if (input.isAPressed()) player1.moveLeft();
-        if (input.isDPressed()) player1.moveRight();
-        if (input.isUpPressed()) player2.moveUp();
-        if (input.isDownPressed()) player2.moveDown();
-        if (input.isLeftPressed()) player2.moveLeft();
-        if (input.isRightPressed()) player2.moveRight();
-    }
-    private void handleShooting() {
-        if (input.isFPressed() && TimerUtil.canShoot(player1.lastShotTime, player1.getWeapon().getCooldown())) {
-            Projectile p = player1.getWeapon().createProjectile(player1);
-            projectiles.add(p);
-            gamePane.getChildren().add(p.getShape());
-            player1.lastShotTime = System.currentTimeMillis();
-        }
-        if (input.isLPressed() && TimerUtil.canShoot(player2.lastShotTime, player2.getWeapon().getCooldown())) {
-            Projectile p = player2.getWeapon().createProjectile(player2);
-            projectiles.add(p);
-            gamePane.getChildren().add(p.getShape());
-            player2.lastShotTime = System.currentTimeMillis();
-        }
     }
     private void updateProjectiles() {
         List<Projectile> toRemove = new ArrayList<>();
