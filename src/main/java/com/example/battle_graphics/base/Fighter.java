4e9118231229d9ab4cc63c1ddbabcc31a1bfa14c
3s3s <eali8242@gmail.com>
2025-12-08 21:58:50 +0200

    push aho

--- src/main/java/com/example/battle_graphics/base/Fighter.java
+++ src/main/java/com/example/battle_graphics/base/Fighter.java
@@ -140,7 +140,6 @@ public abstract class Fighter {
                     currentweapon.getDamage(),
                     facingright,
                     this
-
             );
         }
         return null;
