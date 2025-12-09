ebe007a36e5c2e050302198f29f5d2e6120a9d7a
3s3s <eali8242@gmail.com>
2025-12-10 00:24:05 +0200

    PUSH AHO

--- src/main/java/com/example/battle_graphics/fx/BattleArenaApp.java
+++ src/main/java/com/example/battle_graphics/fx/BattleArenaApp.java
@@ -43,7 +43,7 @@ public class BattleArenaApp extends Application {
         title.setTextFill(Color.web("#ffffff"));
         title.setStyle("-fx-font-weight: bold; -fx-effect: dropshadow(gaussian, rgba(0,0,0,0.6), 6,0,0,2);");
 
-        Label subtitle = new Label("Two-player local combat. Choose fighters and battle!");
+        Label subtitle = new Label("SHOOT AND RUN (ممنوع دخول هنا أحمد!)");
         subtitle.setFont(Font.font("Arial", 18));
         subtitle.setTextFill(Color.web("#dddddd"));
 
